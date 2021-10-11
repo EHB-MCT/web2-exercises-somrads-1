d34f1b7212489770435ec61a48d76b8fce5adc51
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-11 22:06:37 +0200

    proberen om stap 1 af te krijgen

--- ex2/script.js
+++ ex2/script.js
@@ -1,7 +1,8 @@
-import Team from './Teams';
-
+"user strict";
+// import Team from './Teams';
 let list = [];
 let pokemons= [];
+
 fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
 .then(resp => resp.json())
 .then (data => {
@@ -18,20 +19,27 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
 
 });
 
+
 window.onload = function(){
+    console.log("alles loaded");
     setTimeout (buildList, 3000);
 
     function buildList(){
-      const pokeList = document.getElementById("pokelist");
-        console.log(pokemons);
-        const pokemonEl = document.createElement('div');
-        pokemonEl.classList.add('pokemon');
-
-        pokemons.forEach(element => {
-          
+        pokemons.forEach(data => {
+        const pokeContainer = document.getElementById("pokecard");
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+    const pokeInnerHtml = `
+  <div class="img-container">
+  <img src="${data.sprites.front_default}">
+  </div>
+  <div class="pokelist">
+  <h1 class="id">${data.id}</h1>
+  <h3 class="name">${data.name}</h3>
+  <h4 class="type">${data.types.map(type => type.type.name).join(", ")}</h4>
+  </div>
+  `;
+    pokeContainer.innerHTML = pokeInnerHtml;  
         });
-        let htmlString = `
-
-        `
     }   
 }
